f2f341b8ea2075432eec253002d93eacb062929c
nada-r <nadarzer@gmail.com>
2025-06-01 04:34:38 +0200

    change CORS enabling

--- ontology/src/main.ts
+++ ontology/src/main.ts
@@ -3,9 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['*'],  // or '*'' while testing
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
